ae61d6b7c5b478829c10af65fcb38840e150031e
David Soria Parra <dsp@experimentalworks.net>
2019-12-24 23:55:58 -0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 set -x
 set -e
 
-tree_id = awk '{print $3}' <(git ls-tree master -- public)
+tree_id=$(awk '{print $3}' <(git ls-tree master -- public))
 git checkout --recurse-submodule gh-pages
 git read-tree -m -u $tree_id
 git commit -m'autodeploy'
